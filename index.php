ad90a7f87925446dbb681c3f3c6f4465a64365f1
karolz19 <memester@jpmail.xd>
2022-03-23 10:59:50 +0100

    v1.1

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php 
 
-echo "hello world";
+$db = new mysqli("localhost", "root", "", "med");
+$q = $db->prepare("SELECT * FROM staff");
+if($q->execute()) {
+
+    $result = $q->get_result();
+    while($row = $result->fetch_assoc()) {
+        echo $row['firstName'];
+    }
+
+} else {
+    die("zjebałeś kod");
+}
 
 ?>
