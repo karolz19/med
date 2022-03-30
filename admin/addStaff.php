1792d9f36193292642711286322b19e659532faa
karolz19 <memester@jpmail.xd>
2022-03-30 10:05:29 +0200

    v1.34

---
+++ admin/addStaff.php
@@ -0,0 +1,24 @@
+<?php 
+if(isset($_REQUEST['firstName']) && isset($_REQUEST['lastName'])) {
+$db = new mysqli("localhost", "root", "", "med");
+$q = $db->prepare("INSERT INTO staff VALUES(NULL, ?, ?)");
+$q->bind_param("ss", $_REQUEST['firstName'], $_REQUEST['lastName']);
+if($q->execute()) {
+    echo "dodano nowy personel";
+}
+} else {
+    echo '<form action="addStaff.php" method="post">
+    <label for="firstName">Imię</label>
+    <input type="text" name="firstName" id="firstName">
+    <label for="lastName">Nazwisko</label>
+    <input type="text" name="lastName" id="lastName">
+    
+    <input type="submit" value="Dodaj nowy personel">
+    
+    </form>';
+}
+
+?>
+
+
+
