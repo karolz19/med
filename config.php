3a08b8a99961631d5513a371bcf1886869b0ecba
karolz19 <memester@jpmail.xd>
2022-04-06 12:09:43 +0200

    v1.4

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+$db = new mysqli("localhost", "root", "", "med");
+
+require_once("./vendor/autoload.php");
+
+$smarty = new Smarty();
+
+$smarty->setTemplateDir('./templates');
+$smarty->setCompileDir('./templates_c');
+$smarty->setCacheDir('./cache');
+$smarty->setConfigDir('./configs');
+
+$smarty->display("test.tpl");
+
+
+?>
